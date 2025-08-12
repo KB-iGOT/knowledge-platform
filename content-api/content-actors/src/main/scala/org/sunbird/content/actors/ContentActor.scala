3432a9849abda8fd5fc0a35a46ea5519cd9e3029
sahilchaudhary <sahil.chaudhary@tarento.com>
2025-08-12 18:39:14 +0530

    KB-11193 :: Creator | The "languageMapV1" is not updating correctly when simultaneously sent both Kannada and Telugu contents for review.

--- content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
@@ -64,6 +64,7 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 			case "rejectContent" => rejectContent(request)
 			case "adminReadContent" => adminRead(request)
 			case "createMLContent" => createMLContent(request)
+			case "reviewMLContent" => reviewMLContent(request)
 			case _ => ERROR(request.getOperation)
 		}
 	}
@@ -758,5 +759,68 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 				Future.successful(ResponseHandler.OK())
 			}
 		}
+	}
+
+	def reviewMLContent(request: Request)(implicit ec: ExecutionContext): Future[Response] = {
+
+		val identifiers: List[String] = request.getRequest.getOrDefault("identifier", List.empty[String])
+		match {
+			case s: String if StringUtils.isNotBlank(s) => List(s)
+			case arr: Array[String]                     => arr.toList
+			case list: java.util.List[_]                => list.asScala.toList.map(_.toString)
+			case _                                      => List.empty[String]
+		}
+
+		identifiers.foldLeft(Future.successful(())) { (acc, identifier) =>
+				acc.flatMap { _ =>
+					val readReq = new Request(request)
+					readReq.put("identifier", identifier)
+					readReq.put("mode", "edit")
+
+					DataNode.read(readReq).flatMap { node =>
+						if (node != null && StringUtils.isNotBlank(node.getObjectType)) {
+							request.getContext.put("schemaName", node.getObjectType.toLowerCase())
+						}
+
+						if (StringUtils.equalsAnyIgnoreCase("Processing", node.getMetadata.getOrDefault("status", "").asInstanceOf[String])) {
+							Future.failed(new ClientException("ERR_NODE_ACCESS_DENIED", s"Review Operation Can't Be Applied On Node $identifier Under Processing State"))
+						} else {
+							ReviewManager.review(request, node).flatMap { _ =>
+								try {
+									val reviewers = node.getMetadata.get("reviewerIDs") match {
+										case arr: Array[String] => arr.toList
+										case list: java.util.List[_] => list.asScala.toList.map(_.toString)
+									}
+
+									if (reviewers.nonEmpty) {
+										NotificationManager.sendNotification(
+											"CONTENT_REVIEW_REQUEST",
+											"ALERT",
+											reviewers,
+											node.getMetadata.get("name").asInstanceOf[String],
+											Map[String, Any]("id" -> identifier)
+										)
+									} else {
+										logger.warn(s"No reviewers found for content with identifier: $identifier")
+									}
+								} catch {
+									case e: Exception => logger.info("Error while sending notification ", e)
+								}
+
+								val courseCategory = node.getMetadata.get(ContentConstants.COURSE_CATEGORY).asInstanceOf[String]
+
+								logger.info(s"The courseCategory inside review method is: $courseCategory")
+
+								if (StringUtils.isNotBlank(courseCategory) && courseCategory.equalsIgnoreCase(ContentConstants.MULTILINGUAL_COURSE)) {
+									syncLanguageMapStatus(identifier, "Review").map(_ => ())
+								} else {
+									Future.successful(())
+								}
+							}
+						}
+					}
+				}
+			}
+			.map(_ => ResponseHandler.OK())
 	}
 }
