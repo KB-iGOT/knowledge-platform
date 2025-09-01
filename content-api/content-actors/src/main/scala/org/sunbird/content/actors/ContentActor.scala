4572749770dcc99c16ec8fefe1d660a29940ab43
anilkumarkammalapalli <anilkumar.kammalapalli@tarento.com>
2025-09-01 11:28:36 +0530

    KB-11194 & KB-11373 fix: correct notification handling for multilingual courses to ensure creators receive valid updates and prevent irrelevant alerts

--- content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/actors/ContentActor.scala
@@ -230,9 +230,8 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 			val resourceCategoryOpt = Option(node.getMetadata.get("resourceCategory")).map(_.asInstanceOf[String])
 			val primaryCategoryOpt = Option(node.getMetadata.get("primaryCategory")).map(_.asInstanceOf[String])
 			val categoryToCheck = resourceCategoryOpt.filter(_.nonEmpty).orElse(primaryCategoryOpt).getOrElse("")
-			logger.info(s"Using categoryToCheck: $categoryToCheck")
 			//TODO: THIS BLOCK NEED TO BE OPTIMIZE TO HANDLE UPDATE REVIEW STATUS USE CASES.
-			if (StringUtils.isNotBlank(courseCategory) && request.getContext.getOrDefault("sendNotification", Boolean.box(false)).asInstanceOf[Boolean] && !excludedCategories.contains(categoryToCheck)) {
+			if (request.getContext.getOrDefault("sendNotification", Boolean.box(false)).asInstanceOf[Boolean] && !excludedCategories.contains(categoryToCheck) && !reviewStatus.equalsIgnoreCase("Reviewed")) {
 				try {
 					NotificationManager.sendNotification(
 						"CONTENT_EDITED",
@@ -431,6 +430,20 @@ class ContentActor @Inject() (implicit oec: OntologyEngineContext, ss: StorageSe
 			else
 				DataNode.systemUpdate(request, response,"", None)
 		}).map(node => {
+      try {
+        val reviewStatus = Option(request.get("reviewStatus")).map(_.toString).getOrElse("")
+        if (reviewStatus.equalsIgnoreCase("Reviewed")) {
+          NotificationManager.sendNotification(
+            "CONTENT_EDITED",
+            "UPDATE",
+            List(node.getMetadata.get("createdBy").asInstanceOf[String]),
+            node.getMetadata.get("name").asInstanceOf[String],
+            Map[String, Any]("id" -> identifier)
+          )
+        }
+      } catch {
+        case e: Exception => logger.info("Error while sending notification ", e)
+      }
 			ResponseHandler.OK.put("identifier", identifier).put("status", "success")
 		})
 	}
