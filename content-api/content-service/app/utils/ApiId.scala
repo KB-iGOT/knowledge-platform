b39af69c9affe82c4313bb1fb856fdcdaced67eb
ruksanasemeir <ruksana.semeir@tarento.com>
2025-12-08 19:10:23 +0530

    KB-12295| DEV | BE | Content Retirement API implementation

--- content-api/content-service/app/utils/ApiId.scala
+++ content-api/content-service/app/utils/ApiId.scala
@@ -106,5 +106,6 @@ object ApiId {
 
 	val REVIEW_ML_CONTENT = "api.content.ml.review"
 
-	val UPDATE_REVIEW_STATUS_ML_CONTENT = "api.content.ml.update.review"
+  val UPDATE_REVIEW_STATUS_ML_CONTENT = "api.content.ml.update.review"
+  val RETIRE_SCHEDULER_V1: String = "api.content.v1.retire.scheduler"
 }
