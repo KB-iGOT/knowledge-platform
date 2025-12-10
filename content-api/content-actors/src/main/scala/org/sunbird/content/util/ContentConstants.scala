28eb34b0a68091c8e1343385a8bd18170f2768ea
ruksanasemeir <ruksana.semeir@tarento.com>
2025-12-10 16:42:34 +0530

    KB-12295| DEV | BE | Content Retirement API implementation

--- content-api/content-actors/src/main/scala/org/sunbird/content/util/ContentConstants.scala
+++ content-api/content-actors/src/main/scala/org/sunbird/content/util/ContentConstants.scala
@@ -99,7 +99,6 @@ object ContentConstants {
     val ERR_INVALID_DATE_ORDER_MSG = "retirementDate must be greater than lastEnrollmentDate."
     val ERR_CONTENT_PART_OF_COLLECTION = "ERR_CONTENT_PART_OF_COLLECTION"
     val ERR_CONTENT_PART_OF_COLLECTION_MSG = "Content cannot be retired as it is part of a collection."
-
     val ERR_CONTENT_HAS_ACTIVE_PLAN = "ERR_CONTENT_HAS_ACTIVE_PLAN"
     val ERR_CONTENT_HAS_ACTIVE_PLAN_MSG = "Content cannot be retired as it is part of an active learning plan."
 }
